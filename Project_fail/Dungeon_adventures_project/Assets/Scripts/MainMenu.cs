483a79c745c1aeb46720b8d0608e6bd091a3e07e
turcana <turcanast@gmail.com>
2019-03-22 20:51:04 +0300

    v0.3

---
+++ Project_fail/Dungeon_adventures_project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame() {
+		Debug.Log ("Start is done");
+		SceneManager.LoadScene(SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+	public void QuitGame() {
+		Debug.Log ("Quit is done");
+		Application.Quit();
+	}
+}
